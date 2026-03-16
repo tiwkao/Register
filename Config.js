5ac37ac7613676e07edd197fcc358b676866a499
tiwkao <pongsakorn.592040105@gmail.com>
2026-03-16 22:56:42 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    // API Endpoints
+    WEB_APP_URL: 'XXXXXXXXXXXXXXXXXXXXX',
+
+    // LIFF IDs
+    LIFF_ID: {
+        REGISTER: 'XXXXXXXXXXXXXX',  // LIFF ID สำหรับหน้าลงทะเบียน
+        PROFILE: 'XXXXXXXXXXXXXXX'     // LIFF ID สำหรับหน้าโปรไฟล์
+    }
+};
